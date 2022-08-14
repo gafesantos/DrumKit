982cdb6784005dd424002ea60e595c2070ad5a9e
Gabriela <gabrielaferreiras96@gmail.com>
2022-08-14 15:09:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+
+// This area its only to recognize the click on the buttons 
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (i=0; i<numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonTextContent = this.textContent; 
+    makeSound(buttonTextContent); 
+
+  });   
+}
+
+//This area its only to recognize the keyboard 
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  }
+)
+
+
+function makeSound (key) {
+  switch (key) {
+    case "w": 
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play ();
+      break;
+
+    case "a": 
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play ();
+      break;
+
+    case "s": 
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play ();
+      break;
+
+    case "d": 
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play ();
+      break;
+
+    case "j": 
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play ();
+      break;
+
+    case "k": 
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play ();
+      break;
+
+    case "l": 
+        var kickbass = new Audio("sounds/kickbass.mp3");
+        kickbass.play ();
+      break;
+      
+  
+    default: console.log(buttonTextContent);
+  
+  }
+}
+
+
+
+
